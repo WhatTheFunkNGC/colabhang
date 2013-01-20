dffd39e744aaeb63e8dea2de757632630d92c1dc
Will Hoad <williamhoad@gmail.com>
2013-01-20 18:20:37 +0000

    Lister : Setup : Item Adding 1
    added shared item number and interating button

--- lister/scripts/lister.js
+++ lister/scripts/lister.js
@@ -4,12 +4,28 @@
     console.log("Starting Lister");	
     gapi.hangout.onApiReady.add(this.onApiReady.bind(this));	// Add callback
   }	
-  	
+  
+  //-------------------- Listeners -------------------------
+	
+	// script start
   Lister.prototype.onApiReady = function (event) {	
     if (event.isApiReady === true) {	
       console.log("Lister Ready");	
       // we can start doing stuff here	
+	  
+	  document.getElementById("btnAddItem").onclick =	// attach button to function
+        this.btnAddItemClick.bind(this);
+	  
     }	
+  };	
+  
+  //-------------------- Functions -------------------------
+  
+  // On Add Item Button push
+  HangoutDemo.prototype.btnAddItemClick = function () {	
+    var tempLL = gapi.hangout.data.getValue("listLength") || "0"; // get current number of list items
+	tempLL = (parseInt(value, 10) + 1).toString();                // add 1 to value and convert to string 
+    gapi.hangout.data.setValue("listLength", tempLL);	
   };	
   	
   var Lister = new Lister();	
