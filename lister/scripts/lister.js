b4443eb7c29cdc770e3961155e87ca9efaeb14d9
Will Hoad <williamhoad@gmail.com>
2013-01-20 19:09:18 +0000

    Lister : Setup : Display list 1
    basic Function made to display list items.

--- lister/scripts/lister.js
+++ lister/scripts/lister.js
@@ -7,26 +7,46 @@
   
   //-------------------- Listeners -------------------------
 	
-	// script start
-  Lister.prototype.onApiReady = function (event) {	
-    if (event.isApiReady === true) {	
-      console.log("Lister Ready");	
-      // we can start doing stuff here	
+	// script start and loop
+	Lister.prototype.onApiReady = function (event) {	
+		if (event.isApiReady === true) {	
+			console.log("Lister Ready");	
 	  
-	  document.getElementById("btnAddItem").onclick =	// attach button to function
+		document.getElementById("btnAddItem").onclick =		// attach button to function
         this.btnAddItemClick.bind(this);
+		
+		gapi.hangout.data.onStateChanged.add(				// add callback event for list change
+		this.displayListItems.bind(this)
+		);
 	  
-    }	
-  };	
+		}	
+	
+	
+	};	
   
   //-------------------- Functions -------------------------
   
   // On Add Item Button push
-  Lister.prototype.btnAddItemClick = function () {	
-    var tempLL = gapi.hangout.data.getValue("listLength") || "0"; // get current number of list items
-	tempLL = (parseInt(value, 10) + 1).toString();                // add 1 to value and convert to string 
-    gapi.hangout.data.setValue("listLength", tempLL);	
-  };	
-  	
+	Lister.prototype.btnAddItemClick = function () {	
+		var tempLL = gapi.hangout.data.getValue("listLength") || "0"; 	// get current number of list items
+		tempLL = (parseInt(tempLL, 10) + 1).toString();                	// add 1 to value and convert to string 
+		gapi.hangout.data.setValue("listLength", tempLL);				// Commits new item value
+	};	
+  
+	//Display list Items
+	Lister.prototype.displayListItems = function () {	
+		var div, noItems, ul, li, i, l;									// define variables
+		ul = document.createElement("ul");								// create element
+		noItems = gapi.hangout.data.getValue("listLength") || "0";		// get list Length
+		for (i = 0; i < noItems; i++) {
+			li = document.createElement("li");							// Create new element to attach
+			li.innerHTML = "LIST ITEM " + noItems;						// generate list line
+			ul.appendChild(li);												// add list element to end of full list
+		}
+	div = document.getElementById("list");				// get element
+    div.appendChild(ul);								// add List to HTML element
+	};	
+	
+		
   var Lister = new Lister();	
 }(window));
