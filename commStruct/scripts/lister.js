f4dc3be5d6bb4495a32975bdc93bca70ef53072f
Will Hoad <williamhoad@gmail.com>
2013-02-13 17:33:47 +0000

    debugging

--- commStruct/scripts/lister.js
+++ commStruct/scripts/lister.js
@@ -39,7 +39,6 @@
 			e2.appendChild(addIDAddButton(userID,i));					// add Add user sing button
 			e2.appendChild(addIDDelButton(userID,i));					// add Remove user sign button 
 			idListLength = gapi.hangout.data.getValue("listTxt" + i + "listID") || "0";	// get number of users singed to element i
-			console.log("1");
 			for (j = 1; j <= idListLength ; j++) {						// run through User Singed list for element and add image per user
 				e2.appendChild(userPicture(i,j));
 			};
@@ -60,7 +59,9 @@
 	
 	function addListItem (){
 	var div, i, li, li2, e1, e2;
+	console.log("New 1");
 		div = document.getElementById("lister");
+		console.log("new 2");
 		i = gapi.hangout.data.getValue("listTxt") || "0";			// get list Length
 		li = document.createElement("tr");							// Create new element to attach
 			e1 = document.createElement("td");
@@ -68,13 +69,16 @@
 			e1.appendChild(addDelButton(i));							// add delete button
 			e1.appendChild(addAddButton(i));							// add Add button
 			li.appendChild(e1);
+			console.log("new 3");
 			li2 = document.createElement("tr");
 			e2 = document.createElement("td");
 			e2.appendChild(addIDAddButton(userID,i));					// add Add user sing button
 			e2.appendChild(addIDDelButton(userID,i));					// add Remove user sign button 
-			li2.appendChild(e2);			
+			li2.appendChild(e2);
+console.log("new 4");			
 			div.appendChild(li);											// add list element to end of full list	
 			div.appendChild(li2);
+			console.log("new 5");
 	};
 	
 	/* signs user up to list element
