483c89ea09e604e7f76f9ca5e1993e4e8ee6b79d
Will Hoad <williamhoad@gmail.com>
2013-02-19 07:23:18 +0000

    debugging

--- commStruct/scripts/lister.js
+++ commStruct/scripts/lister.js
@@ -152,7 +152,7 @@
 		div = document.getElementById(tableId);
 		i = ((2 * parseInt(itemNo)) - 1).toString();					// use (2N - 1) to select tabe line corectly
 		div.deleteRow(i);
-		div.deleteRow(i + 1);
+		div.deleteRow((parseInt(i) + 1).toString());
 	};	
 		
 	/* Adds a new table row for a new list item
