3e99e8679600e1e28c4a7eed90233b3e3ba5e248
Will Hoad <williamhoad@gmail.com>
2013-02-22 17:51:37 +0000

    debugging

--- commStruct/scripts/lister.js
+++ commStruct/scripts/lister.js
@@ -234,8 +234,8 @@
 		addBut.align = "top";
 		addBut.onclick = function() { 									// on click calls remove function with param targeting the specific line
 				console.log("Add Press " + addBut.id);
-				gapi.hangout.data.setValue("lastListItemAdded", (parseInt(addBut.id.substring(6)) + 1)); 
-				console.log("Add last affected " + (parseInt(addBut.id.substring(6)) + 1));
+				gapi.hangout.data.setValue("lastListItemAdded", addBut.id.substring(6)); 
+				console.log("Add last affected " + (addBut.id.substring(6)));
 				addNewItemToSharedList ("listTxt",(parseInt(addBut.id.substring(6)) + 1)); 					// adds blank list element below selected element
 		}; 
 		return addBut;													// return button element
