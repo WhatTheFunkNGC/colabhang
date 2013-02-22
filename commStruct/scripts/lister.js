5d8fa5c254b081c1e4e3b98e52c12d830ff3a64e
Will Hoad <williamhoad@gmail.com>
2013-02-22 23:57:00 +0000

    debugging

--- commStruct/scripts/lister.js
+++ commStruct/scripts/lister.js
@@ -51,6 +51,10 @@
 		var li4 =  tb.insertRow(2);
 		li4.innerHTML="4";
 		div.appendChild(tb);								// add new List to HTML element
+		
+		div1 = document.getElementById("mainListerTable");
+		var li5 =  div1.insertRow(2);
+		li5.innerHTML="5";
 		console.log("table added");
 		gapi.hangout.data.setValue("lastListItemAdded", "0"); 
 		addNewItemToSharedList ("listTxt",1);
