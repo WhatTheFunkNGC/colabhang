0621e8f13189d1e999032fa42f5f425e8c9544b7
Will Hoad <williamhoad@gmail.com>
2013-03-01 03:52:19 +0000

    debugging

--- commStruct/scripts/convoProfiles.js
+++ commStruct/scripts/convoProfiles.js
@@ -5,8 +5,8 @@ var convoProfiles = [
 			"allowButtingIn" : "true",
 			"muteIfSpeaker" : "false",
 			"userTypes" : [
-				{"name" : "standard", "limit" : "0"},
-				{"name" : "lead", "limit" : "0"}
+				{"name" : "standard", "limit" : "-1"},
+				{"name" : "lead", "limit" : "2"}
 				] },
 			
 		{	"profileName" : "No ButIns", "discription" : "basic profile that gards against butting in",
