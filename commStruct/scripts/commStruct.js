65d5ba97452d7d8b57440a87d0f3b821f8f8b0e5
Will Hoad <williamhoad@gmail.com>
2013-03-08 01:51:38 +0000

    debugging

--- commStruct/scripts/commStruct.js
+++ commStruct/scripts/commStruct.js
@@ -193,7 +193,7 @@
 		btn.value = convoProfiles[profile].userTypes[userProfile].name;	
 		btn.onclick = function() {
 			console.log("USER PROFILE BUT PRESS " + btn.id.substring(20));	
-			var oldTotal, oldTotalNum ,newTotalNum newTotal;
+			var oldTotal, oldTotalNum ,newTotalNum, newTotal;
 			var limit = convoProfiles[currentProfileLoaded].userTypes[btn.id.substring(20)].limit;
 			console.log("limit = " + limit + " and current numbers = " + gapi.hangout.data.getValue("userProfileTotals" + btn.id.substring(20)));	
 			if(limit > gapi.hangout.data.getValue("userProfileTotals" + btn.id.substring(20)) || limit == "-1"){
