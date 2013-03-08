e8add22e7ebfbd6c6e4686a93fdf9258ed99c8a4
Will Hoad <williamhoad@gmail.com>
2013-03-08 23:28:58 +0000

    debugging

--- commStruct/scripts/commStruct.js
+++ commStruct/scripts/commStruct.js
@@ -288,10 +288,9 @@
 		if (setupNeeded) { profileNum = "0";};
 		console.log("load options " + profileNum + "  and " + currentUserProfileLoaded);
 		allowButtingIn = convoProfiles[profileNum].userTypes[currentUserProfileLoaded].allowButtingIn;
+		if(allowButtingIn == "false"){allowButtingIn = false };
 		muteIfSpeaker = convoProfiles[profileNum].userTypes[currentUserProfileLoaded].muteIfSpeaker;
-		console.log("muter is " + muteIfSpeaker);
-		if(muteIfSpeaker){console.log(" muter true"); };
-		if(!muteIfSpeaker){console.log(" muter false"); };
+		if(muteIfSpeaker == "false"){muteIfSpeaker = false };
 		
 		
 	};
