dfa310dbc66af0e7ffcdb3eb798ac7306c962aa6
Will Hoad <williamhoad@gmail.com>
2013-03-13 03:47:05 +0000

    debugging

--- commStruct/scripts/commStruct.js
+++ commStruct/scripts/commStruct.js
@@ -337,7 +337,7 @@
 	// a button for navigating which list item is highlighted
 	function createListNavBtnDown() {
 		var btn = document.createElement("button");
-		btn.innerHTML = "  &#9660  b";
+		btn.innerHTML = "  b"; //&#9660  
 		btn.id = "listHighlightDownBtn";
 		btn.onclick = function() {
 			if(gapi.hangout.data.getValue("currentHighlightedItem") != gapi.hangout.data.getValue("listTxt")){
@@ -351,6 +351,7 @@
 	function createListNavBtnUp() {
 		var btn = document.createElement("button");
 		btn.innerHTML = "  &#9650  a";
+		btn.value = "&#9650";
 		btn.id = "listHighlightUpBtn";
 		btn.onclick = function() {
 			if(gapi.hangout.data.getValue("currentHighlightedItem") != "0"){
